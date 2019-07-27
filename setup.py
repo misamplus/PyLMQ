ff14116fd19ec23ff73536f09e5fc5d59e575b7c
Misam <misamplus@gmail.com>
2019-07-27 16:35:55 +0430

    Import from version 1.2.2

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import re
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with open('libs/python/version.py', 'r', encoding='utf-8') as f:
+    version = re.search(r"^__version__\s*=\s*'(.*)'.*$", f.read(), flags=re.MULTILINE).group(1)
+
+setup(
+    name='PyLMQ',
+    version=version,
+    packages=['PyLMQ'],
+    description='Python Library for LMQ',
+    long_description=long_description,
+    author='Misam saki',
+    author_email='misamplus@gmail.com',
+    url='https://github.com/misamplus/PyLMQ',
+    install_requires=['requests'],
+    license='Proprietary',
+    classifiers=[
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Development',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers'
+    ]
+)
